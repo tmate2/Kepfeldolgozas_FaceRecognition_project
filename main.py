732927e7b5483a1e322ca0c4d6e4338fa153b3a2
Sciencewolf <aron17marton@gmail.com>
2023-11-03 21:57:31 +0100

    Added by aron, refactoring

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import cv2
-import face_recognition as fr   # https://github.com/ageitgey/face_recognition
+import face_recognition as fr  # https://github.com/ageitgey/face_recognition
 
 # hozzáadott modulok "python-opencv", "face-recognition", "dlib"
 
@@ -8,45 +8,7 @@ import face_recognition as fr   # https://github.com/ageitgey/face_recognition
 KAMERA = 1
 
 
-def main():
-    capture = cv2.VideoCapture(KAMERA)
-
-    # képek előkészítése az arcok felismeréséhez
-    face1 = fr.load_image_file("known_faces/MateThold.jpg")
-    face1_enc = fr.face_encodings(face1)[0]
-
-    face2 = fr.load_image_file("known_faces/TerryDavis.jpg")
-    face2_enc = fr.face_encodings(face2)[0]
-
-    known_faces_enc = [
-        face1_enc,
-        face2_enc
-    ]
-
-    # az arcokhoz tartozó nevek eltárolása
-    names = [
-        "Mate Thold",
-        "Terry Davis"
-    ]
-
-    # a kamera képének a megjelenítéséhez egy végtelenített függvényt használunk
-    while True:
-        is_true, frame = capture.read()
-
-        # az aktuális framen arcokat keresünk majd annak az így kapott és esetlegesen megjelölt képet megjelenítjük
-        frame = face_detect(frame, known_faces_enc, names)
-        cv2.imshow("Face detector", frame)
-
-        # a 'd' billentyű lenyomásával megszakítjuk a végtelenciklust, ezáltal "leállítjuk a felvételt"
-        if cv2.waitKey(20) & 0xFF == ord('d'):
-            break
-
-    capture.release()
-    cv2.destroyAllWindows()
-
-
 def face_detect(frame, known_faces_enc, names):
-
     # arcok helyének elmentése az aktuális képkocáról
     faces = fr.face_locations(frame)  # várható eredmény: [(284, 324, 469, 139)]
 
@@ -75,6 +37,8 @@ def face_detect(frame, known_faces_enc, names):
     # Ha két arc közel van egymáshoz vagy összeérnek a koordinátái, "IndexError"-t dob
     # a 'name = names[known_faces_name[index]' sorba és leáll a program (exit code 1)
 
+    handle_index_error(known_faces_index, known_faces_name, faces)  # az alatti resz kod majd ebben a metodusban lesz
+
     # arc körberajzolása, ismeretlen arc pirossal,
     # ismert arcokat zölddel, alattuk a hozzá tartozó névvel
     for index, (top, right, bottom, left) in enumerate(faces):
@@ -82,21 +46,67 @@ def face_detect(frame, known_faces_enc, names):
             # körberajzolás
             cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 4)
             # névmező létrehozása
-            cv2.rectangle(frame, (left-2, bottom), (right+2, bottom+23), (0, 255, 0), cv2.FILLED)
+            cv2.rectangle(frame, (left - 2, bottom), (right + 2, bottom + 23), (0, 255, 0), cv2.FILLED)
 
             # szöveg méretének igazítása az arc széllességéhez
-            font_scale = right-left
+            font_scale = right - left
             if font_scale >= 200:
                 font_scale = 1
             else:
                 font_scale /= 200
             # archoz tartozó név kiírása a névmezőbe
             name = names[known_faces_name[index]]
-            cv2.putText(frame, name, (left+6, bottom+21), cv2.FONT_HERSHEY_TRIPLEX,  font_scale, (0, 0, 0), 1)
+            cv2.putText(frame, name, (left + 6, bottom + 21), cv2.FONT_HERSHEY_TRIPLEX, font_scale, (0, 0, 0), 1)
         else:
             cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 4)
 
     return frame
+
+
+def handle_index_error(known_faces_index: list, knows_faces_name: list, faces: list):
+    pass
+
+
+def main():
+    capture = cv2.VideoCapture(KAMERA)
+
+    # képek előkészítése az arcok felismeréséhez
+    face1 = fr.load_image_file("known_faces/MateThold.jpg")
+    face1_enc = fr.face_encodings(face1)[0]
+
+    face2 = fr.load_image_file("known_faces/TerryDavis.jpg")
+    face2_enc = fr.face_encodings(face2)[0]
+
+    face3 = fr.load_image_file("known_faces/JoeBiden.jpg")
+    face3_enc = fr.face_encodings(face3)[0]
+
+    known_faces_enc = [
+        face1_enc,
+        face2_enc,
+        face3_enc
+    ]
+
+    # az arcokhoz tartozó nevek eltárolása
+    names = [
+        "Mate Thold",
+        "Terry Davis",
+        "Joe Biden"
+    ]
+
+    # a kamera képének a megjelenítéséhez egy végtelenített függvényt használunk
+    while True:
+        is_true, frame = capture.read()
+
+        # az aktuális framen arcokat keresünk majd annak az így kapott és esetlegesen megjelölt képet megjelenítjük
+        frame = face_detect(frame, known_faces_enc, names)
+        cv2.imshow("Face detector", frame)
+
+        # a 'd' billentyű lenyomásával megszakítjuk a végtelenciklust, ezáltal "leállítjuk a felvételt"
+        if cv2.waitKey(20) & 0xFF == ord('d'):
+            break
+
+    capture.release()
+    cv2.destroyAllWindows()
 
 
 if __name__ == "__main__":
